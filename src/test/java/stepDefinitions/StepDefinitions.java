01fbc1a0cb144c511fe5a471c19345cd4c3e63c7
srikanth <srikanthv1709@gmail.com>
2025-02-11 19:15:26 +0530

    feb 11

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -1,14 +1,47 @@
 package stepDefinitions;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import pojo.AddPlace;
+import pojo.Location;
 
 public class StepDefinitions {
 	@Given("Add Place Payload")
 	public void add_place_payload() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		AddPlace addPlace = new AddPlace();
+		addPlace.setAccuracy(50);
+		addPlace.setAddress("29, side layout, cohen 09");
+		addPlace.setLanguage("French-IN");
+		addPlace.setPhone_number("(+91) 983 893 3937");
+		addPlace.setWebsite("https://rahulshettyacademy.com");
+		addPlace.setName("Frontline house");
+		List<String> myList = new ArrayList<String>();
+		myList.add("shoe park");
+		myList.add("shop");
+		addPlace.setTypes(myList);
+		Location location = new Location();
+		location.setLat(-38.383494);
+		location.setLng(33.427362);
+		addPlace.setLocation(location);
+		RequestSpecification requestSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addQueryParam("key", "qaclick123").setContentType(ContentType.JSON).build();
+		
+		ResponseSpecification resSpec = new ResponseSpecBuilder().expectStatusCode(200)
+				.expectContentType(ContentType.JSON).build();
+		RequestSpecification res = given().spec(requestSpec).body(addPlace);
 	}
 
 	@When("User calls {string} with Post http request")
@@ -28,6 +61,5 @@ public class StepDefinitions {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
-
 
 }
